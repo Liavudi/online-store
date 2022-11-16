e56bec153f1d2dc559b1dae8a57e385f31e6aa9b
Liavudi <liavudi@gmail.com>
2022-11-17 00:26:37 +0200

    first commit

---
+++ frontend/src/components/nav-bar/nav-bar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './nav-bar.css'
+
+export const NavBar = () => {
+  return (
+    <div className='container'>
+        <input className='search-bar'></input>
+        <button className='search-button'>Search</button>
+    </div>
+    
+  )
+}
---
+++ frontend/src/pages/homepage.js
@@ -0,0 +1,10 @@
+import { NavBar } from "../components/nav-bar/nav-bar";
+
+
+export default function Homepage() {
+  return (
+   <div>
+    <NavBar />
+    </div>
+  );
+}
