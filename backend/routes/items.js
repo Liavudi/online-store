66987f173be68f0709cb06a9b95aa7c4b9e61310
Liavudi <liavudi@gmail.com>
2022-11-20 12:59:08 +0200

    updated items in the server [GET getbyid,DELETE deletebyid,PUT updatebyid]

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -30,5 +30,46 @@ router.post("/", async (req, res) => {
   res.send(item);
 });
 
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const item = await Item.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      catalog: req.body.catalog,
+      category: req.body.category,
+      image: req.body.image,
+    },
+    { new: true }
+  );
+
+  if (!item)
+    return res.status(404).send("The item with the given ID was not found.");
+
+  res.send(item);
+});
+
+router.delete("/:id", async (req, res) => {
+  const item = await Item.findByIdAndRemove(req.params.id);
+
+  if (!item)
+    return res.status(404).send("The item with the given ID was not found.");
+
+  res.send("Item was successfully removed");
+});
+
+router.get("/:id", async (req, res) => {
+  const item = await Item.findById(req.params.id);
+
+  if (!item)
+    return res.status(404).send("The item with the given ID was not found.");
+
+  res.send(item);
+});
 
 module.exports = router;
