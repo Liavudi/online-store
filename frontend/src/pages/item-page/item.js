cbd3b9f5bf8463da21665e9c0841296f146d5d2b
Liavudi <liavudi@gmail.com>
2022-11-20 17:44:25 +0200

    additional css changes

--- frontend/src/pages/item-page/item.js
+++ frontend/src/pages/item-page/item.js
@@ -13,12 +13,14 @@ export const Item = () => {
   return (
     <div className="container">
       <div className="item-container">
-        <img className="image-size" src={itemDetails.image} />
-        <label>{itemDetails.name}</label>
-
-        <label className='description'>{itemDetails.description}</label>
-
-        <label>₪{itemDetails.price}</label>
+        <img draggable={false} className="image-size" src={itemDetails.image} />
+        <label className='item-name'>{itemDetails.name}</label>
+        <label className="description">{itemDetails.description}</label>
+        <div className='item-order-options'>
+          <button className='order-button'>Buy Now!</button>
+          <button className='order-button'>Basket</button>
+          <label>₪{itemDetails.price}</label>
+        </div>
       </div>
     </div>
   );
