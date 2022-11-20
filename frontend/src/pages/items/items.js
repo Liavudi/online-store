2ec8f85a6868cc838ca33850dc4a71174564544a
Liavudi <liavudi@gmail.com>
2022-11-20 15:29:59 +0200

    added css for the items page

--- frontend/src/pages/items/items.js
+++ frontend/src/pages/items/items.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Footer from '../../components/footer/footer';
-import { NavBar } from '../../components/nav-bar/nav-bar';
-import utils from '../../api/utils';
-
-
-  
-
+import Footer from "../../components/footer/footer";
+import { NavBar } from "../../components/nav-bar/nav-bar";
+import utils from "../../api/utils";
+import './items.css'
 
 export default function Items() {
-    const [allCategoryItems, setAllCategoryItems] = useState([])
-    const params = useParams()
-    
-    useEffect(() => {
-        utils.getItemsByCategory(params.category).then(res => setAllCategoryItems(res.data))
-    },[])
-    
-    const categoryContent = allCategoryItems.map((data, key) => {
-        return <img key={key} src={data.image}/>
-        
-    })
-        
+  const [allCategoryItems, setAllCategoryItems] = useState([]);
+  const params = useParams();
 
+  useEffect(() => {
+    utils
+      .getItemsByCategory(params.category)
+      .then((res) => setAllCategoryItems(res.data));
+  }, []);
 
+  const categoryContent = allCategoryItems.map((data, key) => {
     return (
-        <>
-            <div className="all-content-but-footer">
-            <NavBar />
+      <div key={key} className='item'>
+        <img className='image-size' src={data.image} />
+        <label>{data.name}</label>
+        <label className='item-description'>{data.description}</label>
+        <label>₪{data.price}</label>
+        
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="all-content-but-footer">
+        <NavBar />
+        <div className='items-container'>
             {categoryContent}
-            </div>
-            <Footer />
-        </>
-    )
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
